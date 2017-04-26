bbe49ccfad2ac6b7d149810385c9cbcfbcae9ed3
nathanrd <nathanael.dungca@hotmail.com>
2017-04-26 11:19:46 +0200

    norsk

---
+++ Template/directory/lang.no.php
@@ -0,0 +1,35 @@
+<?php
+/* 
+------------------
+Language: Norsk
+------------------
+*/
+
+$lang = array();
+$lang['PAGE_TITLE'] = 'Forside';
+
+// Menu
+$lang['MENU_HOME'] = 'Hjem';
+$lang['MENU_MAP'] = 'Kart';
+$lang['MENU_ EVENTS'] = 'Eventer';
+$lang['MENU_CAMPUS'] = 'Campus';
+$lang['MENU_SEARCH'] = 'Søk her';
+$lang['MENU_SUBMIT'] = 'Søk';
+
+// Homepage
+$lang['HOMEPAGE_FIRST_TITLE'] = 'Nyheter';
+$lang['HOMEPAGE_SECOND_TITLE'] = 'Eventer';
+
+
+// Footer
+$lang['FOOTER_NAVIGATION'] = 'Navigasjon';
+$lang['FOOTER_NAVIGATION_HOME'] = 'Hjem'; 
+$lang['FOOTER_NAVIGATION_MAP'] = 'Kart';  
+$lang['FOOTER_NAVIGATION_EVENTS'] = 'Eventer';
+$lang['FOOTER_NAVIGATION_CAMPUS'] = 'Campus';
+$lang['FOOTER_NAVIGATION_SUPPORT'] = 'Support';
+$lang['FOOTER_ADRESS'] = 'Adresse';
+$lang['FOOTER_CONTACT_TITLE'] = 'Kontakt oss';
+$lang['FOOTER_CONTACT_CONTENT'] = 'Har du et spørsmål eller noe vi kan forbedre oss på? Kontakt oss her';
+$lang['FOOTER_CONTACT_LINK'] = 'Kontakt';
+
