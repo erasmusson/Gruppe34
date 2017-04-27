1c8a341827a957be4dfec8cc1d9a7e9d6bb22603
nathanrd <nathanael.dungca@hotmail.com>
2017-04-27 12:02:59 +0200

    type=text/css ...

--- Template/index.php
+++ Template/index.php
@@ -27,10 +27,10 @@
     <title><?php echo $lang['PAGE_TITLE']; ?></title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
       
     <!-- Custom CSS -->
-    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet" type="text/css">
       
   </head>
   <body>
