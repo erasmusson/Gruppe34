54fe0df8465d319e9ad36c8abbf7a1dea2ab6067
Emma Rasmusson <rasemm16@student.westerdals.no>
2017-04-27 12:07:55 +0200

    eventsslide för index är klar

--- Template/events/EventsSlide.php
+++ Template/events/EventsSlide.php
@@ -1,76 +1,45 @@
-    <div class="col-md-12">
-              <h1>Events</h1>
-              
-              <div class="well">
-                <h4>Highlighted events</h4>
-                  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
-                      <div class="carousel-inner">
-                          <div class="item active">
-                        <div class="row">
-                            <?php  
-                            
-                                $i = 1;
-                                $x = 5;
-                            
-                                while($i < $x) {
-                                    $statement = $connection->query("SELECT * FROM events where id = '$i'"); 
-                                    $row = $statement->fetch(PDO::FETCH_ASSOC);
+<!-- Title of events slide-->
+<h1>Highlighted events</h1>
+
+<!-- Row start -->
+<div class="row">
+
+    <!-- Loop that gets 4 highlighted future events -->                            
+    <?php 
+        $i = 0;
+        $counter = 0;
+        
+        while($i < 4){
+            $a = $events[$counter];                    
+            if($a['starts_at']->isFuture() && $a['highlighted'] == 1) { ?>
+
+                <div class="col-md-3 eventcard">
+                    <img src="<?= $a['image_path'] ?>" class="image-responsive"  style="height: 200px;">
+                    <h4><?= $a['title'] ?></h4>
+                    <p><?= $a['starts_at'] ?></p>
+                    <p><?= $a['location'] ?></p>
+                </div> 
                                     
-                                    if($row['starts_at']->isFuture()){
-                                    ?>
-                                    
-                                       <div class="col-sm-3">
-                                        <img src="<?= $row['image_path']?>" alt="Image" class="img-responsive" style="height:200px;">
-                                        <h5><?= $row['title'] ?></h5>
-                                    </div>
-                                
-                                    <?php
-                                        
-                                      $i++;
-                                    }
-                                    else{$i++; $x++;}
-                                    }
-                                    ?>
-                            
-                            
-                        </div>
+                <?php
+                   $i++;
+                   $counter++;
+                } 
+
+                else{
+                    $counter++;     
+                } 
+            }                                 
+    ?>
+
+<!-- End of row -->
+</div>
                               
-                        <!--/row-->
-                    </div>
-                    <!--/item-->
-                    <div class="item">
-                        <div class="row">
-                            <div class="col-sm-3"><a href="#x"><img src="http://placehold.it/250x250" alt="Image" class="img-responsive"></a>
-                            </div>
-                            <div class="col-sm-3"><a href="#x"><img src="http://placehold.it/250x250" alt="Image" class="img-responsive"></a>
-                            </div>
-                            <div class="col-sm-3"><a href="#x"><img src="http://placehold.it/250x250" alt="Image" class="img-responsive"></a>
-                            </div>
-                            <div class="col-sm-3"><a href="#x"><img src="http://placehold.it/250x250" alt="Image" class="img-responsive"></a>
-                            </div>
-                        </div>
-                        <!--/row-->
-                    </div>
-                    <!--/item-->
-                    <div class="item">
-                        <div class="row">
-                            <div class="col-sm-3"><a href="#x"><img src="http://placehold.it/250x250" alt="Image" class="img-responsive"></a>
-                            </div>
-                            <div class="col-sm-3"><a href="#x"><img src="http://placehold.it/250x250" alt="Image" class="img-responsive"></a>
-                            </div>
-                            <div class="col-sm-3"><a href="#x"><img src="http://placehold.it/250x250" alt="Image" class="img-responsive"></a>
-                            </div>
-                            <div class="col-sm-3"><a href="#x"><img src="http://placehold.it/250x250" alt="Image" class="img-responsive"></a>
-                            </div>
-                        </div>
-                        <!--/row-->
-                    </div>
-                    <!--/item-->
-                </div>
-                      <a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-                      <a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
-                  </div>
-              </div>
-              
-          </div>
+
+
+                      
+                  
+             
+
+        
+
 
