3a4df8e54450325fd1c19426b49b1ef2c68d39e7
Emma Rasmusson <rasemm16@student.westerdals.no>
2017-05-28 14:55:23 +0200

    Kommentering

--- photocred.php
+++ photocred.php
@@ -1,83 +1,92 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html lang="no">
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Photocredit</title>
+    <title>Bildcredit | Campus Fjerdingen </title>
+        <link rel="icon" href="pic/logo_bw_1_SYy_icon.ico">
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-      
+
     <!-- Custom CSS -->
     <link href="css/custom.css" rel="stylesheet">
-      
-      <style>
-        #photoCreditContainer img{
-    display: inline;
-    height: 40px;
-    
-}
 
-#photoCreditContainer div{
-    display: inline;
-}
+</head>
 
-      </style>
-  </head>
-  <body>
-      
-        <!-- Fetches all parts of events -->
-      <div class="container-fluid">
+<body>
+
+    <!-- Fetches all parts of events -->
+    <div class="container-fluid">
         <?php require 'navbar.php' ?>
-      </div>
-      
-      <div class="container">
-      
+    </div>
+
+    <div class="container">
+
         <div class="row">
 
             <div id="photoCreditContainer" class="col-md-12">
                 <h3><?php echo $lang['PHOTOCRED_TITLE']; ?></h3>
-                
-                <img src="pic/masks.png">
-                <div>Icons made by <a href="http://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><br><br>
 
-                <img src="pic/musikk.png"> <img src="pic/refresh.png">  <img src="pic/football.png"> <img src="pic/drink.png"><br>
-                <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><br><br>
+                <img alt="Bild av mask" src="pic/masks.png">
+                <div>Icons made by <a href="http://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <br>
+                <br>
 
-                <img src="pic/camera.png">
-                <div>Icons made by <a href="http://www.flaticon.com/authors/dimi-kazak" title="Dimi Kazak">Dimi Kazak</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><br><br>
-                
-                <img src="pic/concert.png">
-                <div>Icons made by <a href="http://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><br><br>
-                
-                <img src="pic/workout.png">
-                <div>Icons made by <a href="http://www.flaticon.com/authors/scott-de-jonge" title="Scott de Jonge">Scott de Jonge</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><br><br>
+                <img alt="Bild av Not" src="pic/musikk.png"> <img src="pic/refresh.png"> <img src="pic/football.png"> <img src="pic/drink.png">
+                <br>
+                <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <br>
+                <br>
 
-                <img src="pic/music.png">
-                <div>Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><br><br>
+                <img alt="Bild av Kamera" src="pic/camera.png">
+                <div>Icons made by <a href="http://www.flaticon.com/authors/dimi-kazak" title="Dimi Kazak">Dimi Kazak</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <br>
+                <br>
 
-                <img src="pic/ambulance.png">
-                <div>Icons made by <a href="http://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><br><br>
+                <img alt="Bild av konsert" src="pic/concert.png">
+                <div>Icons made by <a href="http://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <br>
+                <br>
 
-                <img src="pic/drugs.png"> 
-                <div>Icons made by <a href="http://www.flaticon.com/authors/retinaicons" title="Retinaicons">Retinaicons</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><br><br>
+                <img alt="Bild av dumbbell" src="pic/workout.png">
+                <div>Icons made by <a href="http://www.flaticon.com/authors/scott-de-jonge" title="Scott de Jonge">Scott de Jonge</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <br>
+                <br>
+
+                <img alt="Bild av not" src="pic/music.png">
+                <div>Icons made by <a href="http://www.flaticon.com/authors/madebyoliver" title="Madebyoliver">Madebyoliver</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <br>
+                <br>
+
+                <img alt="Bild av ambulanse" src="pic/ambulance.png">
+                <div>Icons made by <a href="http://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <br>
+                <br>
+
+                <img alt="Bild av piller" src="pic/drugs.png">
+                <div>Icons made by <a href="http://www.flaticon.com/authors/retinaicons" title="Retinaicons">Retinaicons</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                <br>
+                <br>
 
                 <?php echo $lang['PHOTOCRED_WESTERDALS']; ?>
-                
+
             </div>
         </div>
-      
-      </div>
-      
-        <div class="push"></div>
-        
-        <!-- Fetches footer -->
-        <?php require 'footer.php' ?>
-      
-    <!-- jquery og bootstrap script -->  
+
+    </div>
+
+    <div class="push"></div>
+
+    <!-- Fetches footer -->
+    <?php require 'footer.php' ?>
+
+        <!-- jquery og bootstrap script -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/script.js"></script>
-  </body>
+</body>
+
 </html>
